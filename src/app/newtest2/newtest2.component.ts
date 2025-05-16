1c013de1c012f53d688aec298b78039919a7633d
Naveen <navtec41@gmail.com>
2025-05-16 12:19:49 +0530

    newtest2  commit

---
+++ src/app/newtest2/newtest2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newtest2',
+  imports: [],
+  templateUrl: './newtest2.component.html',
+  styleUrl: './newtest2.component.css'
+})
+export class Newtest2Component {
+
+}
