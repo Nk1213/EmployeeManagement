fd9b431135d5c3b455e3f6dacc5f514efbbe04a5
Naveen <navtec41@gmail.com>
2025-05-16 12:30:08 +0530

    newtest3 changes

---
+++ src/app/newtest3/newtest3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newtest3',
+  imports: [],
+  templateUrl: './newtest3.component.html',
+  styleUrl: './newtest3.component.css'
+})
+export class Newtest3Component {
+
+}
